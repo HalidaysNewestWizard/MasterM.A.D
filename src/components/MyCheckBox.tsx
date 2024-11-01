ee73b02909f11c36ba48af77f86231fd6bcd8fd5
Garth Carey <Garthcarey0862@gmail.com>
2024-11-01 09:52:11 -0600

    Refactor MyCheckBox component and enhance password generation utility

--- src/components/MyCheckBox.tsx
+++ src/components/MyCheckBox.tsx
@@ -1,69 +1,62 @@
-import React,{useState} from 'react';
-import {Text,View ,StyleSheet} from 'react-native';
-import BouncyCheckbox from "react-native-bouncy-checkbox";
+import React, {useState} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
-type CheckboxProps=
-{
-    title:string,
-    checkedStatus : boolean,
-   updateCheckedStatus : (checkedStatus:boolean) => void,
+type CheckboxProps = {
+  title: string;
+  checkedStatus: boolean;
+  updateCheckedStatus: (checkedStatus: boolean) => void;
+};
+function MyCheckBox(props: CheckboxProps) {
+  //Local state lifted to parent Main() component
+  //const [checked,setChecked]=useState(false);
 
+  //Teacher way^^ ????ask why my way  works too ????
+  const checked = props.checkedStatus;
+  const isCheckboxChecked = props.updateCheckedStatus;
+  const title = props.title;
+
+  return (
+    <View style={styles.containerView}>
+      <BouncyCheckbox
+        style={styles.check}
+        isChecked={checked}
+        size={25}
+        fillColor="red"
+        unFillColor="#FFFFFF"
+        iconStyle={{borderColor: 'red'}}
+        innerIconStyle={{borderWidth: 2}}
+        textStyle={{fontFamily: 'JosefinSans-Regular'}}
+        onPress={(isChecked: boolean) => {
+          console.log(title + ' is ' + isChecked);
+          isCheckboxChecked(isChecked);
+        }}></BouncyCheckbox>
+      <Text style={styles.titleTxt}>{title}</Text>
+    </View>
+  );
 }
-function MyCheckBox(props : CheckboxProps){
-    //Local state lifted to parent Main() component
-    //const [checked,setChecked]=useState(false);
+const styles = StyleSheet.create({
+  containerView: {
+    marginHorizontal: 10,
+    marginVertical: 5,
+    width: 300,
 
-    //Teacher way^^ ????ask why my way  works too ????
-    const checked =  props.checkedStatus;
-    const isChecked = props.updateCheckedStatus;
-    const title = props.title;
-
-
-    return(
-        <View style={styles.containerView}>
-        <BouncyCheckbox
-          style={styles.check}
-         
-          isChecked={checked}
-          size={25}
-          fillColor="red"
-          unFillColor="#FFFFFF"
-          iconStyle={{ borderColor: "red" }}
-          innerIconStyle={{ borderWidth: 2 }}
-          textStyle={{ fontFamily: "JosefinSans-Regular" }}
-          onPress={(isChecked: boolean) => {console.log(title + ' is ' + isChecked);}}
-        >
-
-        </BouncyCheckbox>
-        <Text style={styles.titleTxt}>{title}</Text>
-        </View>
-        );
-    }
-const styles =StyleSheet.create({
-    containerView:{
-        marginHorizontal:10,
-        marginVertical:5,
-        width:300,
-
-        flexDirection:'row',
-        borderWidth:4,
-        borderRadius:10,
-         backgroundColor:'#f0f8ff',
-        },
-    titleTxt:{
-        textAlign:'center',
-        marginVertical:5,
-        marginHorizontal:50,
-        fontSize:15,
-        fontWeight:'600',
-        color:'black'
-
-        },
-    check:{
-        margin:10,
-        width:30,
-
-        }
-
-    });
+    flexDirection: 'row',
+    borderWidth: 4,
+    borderRadius: 10,
+    backgroundColor: '#f0f8ff',
+  },
+  titleTxt: {
+    textAlign: 'center',
+    marginVertical: 5,
+    marginHorizontal: 50,
+    fontSize: 15,
+    fontWeight: '600',
+    color: 'black',
+  },
+  check: {
+    margin: 10,
+    width: 30,
+  },
+});
 export default MyCheckBox;
--- src/utility/passwordGenerator.ts
+++ src/utility/passwordGenerator.ts
@@ -1,31 +1,31 @@
-import * as utils from "./Consts";
+import * as utils from './Consts';
 
- export const createPassword = (characters: string, passwordLength: number) : string => {
-    console.log("Generating secured password !!!");
-    let result = "";
-    for(let i=0; i<passwordLength; i++) {
-        const idx = Math.round(Math.random() * characters.length);
-        result += characters.charAt(idx);
-    }
-    console.log(result);
-    return result;
-}
+export const createPassword = (
+  characters: string,
+  passwordLength: number,
+): string => {
+  console.log('Generating secured password !!!');
+  let result = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const idx = Math.round(Math.random() * characters.length);
+    result += characters.charAt(idx);
+  }
+  console.log(result);
+  return result;
+};
 
+export const generatePasswordString = (
+  passReq: utils.PasswordRequirement,
+): string => {
+  let characters = '';
+  if (passReq.includeUpper) characters += utils.CHARACTERS_UPPERCASE;
+  if (passReq.includeLower) characters += utils.CHARACTERS_LOWERCASE;
+  if (passReq.includeNumber) characters += utils.CHARACTERS_NUMBERS;
+  if (passReq.includeSymbol) characters += utils.CHARACTERS_SYMBOL;
 
-export const generatePasswordString = (passReq : utils.PasswordRequirement) : string => {
-    let characters = '';
-    if(passReq.includeUpper) 
-        characters += utils.CHARACTERS_UPPERCASE;
-    if(passReq.includeLower)
-        characters += utils.CHARACTERS_LOWERCASE;
-    if(passReq.includeNumber)
-        characters += utils.CHARACTERS_NUMBERS;
-    if(passReq.includeSymbol)
-        characters += utils.CHARACTERS_SYMBOL;
+  console.log('Characters: ', characters);
 
-    console.log("Characters: ", characters);
-    
-    const generatedPassword = createPassword(utils.CHARACTERS_UPPERCASE, passReq.length);
+  const generatedPassword = createPassword(characters, passReq.length);
 
-    return generatedPassword;
-}
+  return generatedPassword;
+};
